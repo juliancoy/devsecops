688783f4696e05b3ddf747788f2101df00281a58
Julian Loiacono <jcloiacon@gmail.com>
2024-12-07 02:08:56 -0500

    ct3

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@ docker run --rm --net=host \
   -v "$(pwd)":/app \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -w /app \
-  python:3.12 sh -c "pip install docker && python3 run.py"
+  -e GITHUB_ACTIONS=$GITHUB_ACTIONS \
+  python:3.12 bash -c "pip install docker && python3 run.py"
