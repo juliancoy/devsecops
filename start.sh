65c1d991685a950746fbd736cacb39ca087d8602
Julian Loiacono <jcloiacon@gmail.com>
2024-12-07 18:33:13 -0500

    runs setup directly on server

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
-docker run --rm --net=host \
-  --name="master" \
-  -v "$(pwd)":/app \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -w /app \
-  -e GITHUB_ACTIONS=$GITHUB_ACTIONS \
-  -e CURRENT_DIR=$(pwd) \
-  -v /usr/bin/docker:/usr/bin/docker \
-  python:3.12 bash -c "pip install docker && python3 run.py"
+apt update
+apt install python3 
+apt install python3-docker
+python3 run.py
