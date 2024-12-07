2c9d8854e6d3f9d6c07f87a5bbad584b7f4744ab
Julian Loiacono <jcloiacon@gmail.com>
2024-12-07 02:11:45 -0500

    4

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ docker run --rm --net=host \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -w /app \
   -e GITHUB_ACTIONS=$GITHUB_ACTIONS \
+  -v /usr/bin/docker:/usr/bin/docker \
   python:3.12 bash -c "pip install docker && python3 run.py"
